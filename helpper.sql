2314a06da324540e05025da7dd169789f8b5ebca
Jonathan Benicio de Lima Oliveira <45011024+JonathanBenicio@users.noreply.github.com>
2020-06-05 04:14:23 -0300

    Update helpper.sql

--- helpper.sql
+++ helpper.sql
@@ -21,7 +21,7 @@ SET time_zone = "+00:00";
 --
 -- Banco de dados: `helpper`
 --
-CREATE DATABASE helpper
+CREATE DATABASE helpper;
 -- --------------------------------------------------------
 
 --
